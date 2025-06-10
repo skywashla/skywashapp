b55991dd54314fad75644ab5cee038fca75caaee
skywashla <skywashlatam@gmail.com>
2025-06-10 16:36:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import flask, render_template, request, redirect, url_for, session
 import os
 from datetime import datetime
 
